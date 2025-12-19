81d84e4b518a925362d7f053afca39ab39bd2967
Satyakam Pandya <satyakampandya@gmail.com>
2025-12-19 11:26:39 +0530

    Implement deck arrangement via reverse pattern simulation

--- lib/arrange_deck.rb
+++ lib/arrange_deck.rb
@@ -1,5 +1,19 @@
 # frozen_string_literal: true
 
-def arrange_deck(input_pattern, desired_reveal)
-  raise NotImplementedError, 'arrange_deck is not implemented yet'
+def arrange_deck(pattern, desired_order)
+  deck = []
+  reveal_index = desired_order.length - 1
+
+  pattern.reverse.each do |action|
+    if action == 1
+      # Reverse of moving top card to bottom
+      deck.unshift(deck.pop) unless deck.empty?
+    else
+      # Reverse of revealing the top card
+      deck.unshift(desired_order[reveal_index])
+      reveal_index -= 1
+    end
+  end
+
+  deck
 end
