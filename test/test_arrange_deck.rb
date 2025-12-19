9a18463d04bd64a97652c6024589b04fa7522c27
Satyakam Pandya <satyakampandya@gmail.com>
2025-12-19 11:46:15 +0530

    Add tests for invalid pattern actions and desired_order types

--- test/test_arrange_deck.rb
+++ test/test_arrange_deck.rb
@@ -27,6 +27,28 @@ class ArrangeDeckTest < Minitest::Test
     assert_match(/got\s+2/i, error.message)
   end
 
+  def test_desired_order_must_be_array_of_strings
+    desired_order = [:A, 2, '3'] # invalid types
+    pattern = [1, 0, 1, 0, 1, 0]
+
+    error = assert_raises(ArgumentError) do
+      arrange_deck(pattern, desired_order)
+    end
+
+    assert_match(/desired_order must be an array of strings/i, error.message)
+  end
+
+  def test_pattern_with_unknown_action_raises_error
+    desired_order = %w[A 2 3]
+    pattern = [1, 0, 2, 0, 1, 0] # invalid action: 2
+
+    error = assert_raises(ArgumentError) do
+      arrange_deck(pattern, desired_order)
+    end
+
+    assert_match(/pattern contains invalid action/i, error.message)
+  end
+
   private
 
   # Forward simulator used only for validation in tests
